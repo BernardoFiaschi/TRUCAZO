43d0039bc9456eae7536b8a77e2bbb3b67e91385
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2025-06-07 22:30:05 -0300

    Delete imagenes.h

--- imagenes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Carta.h"
-
-// Mostramos las cartas del jugador de forma visual usando SFML //
-
-// Recibimos un arreglo de cartas y la cantidad actual del jugador //
-
-void mostrarCartasJugadorVisual(const Carta cartasJugador[], int cantidad);
